cf2bf5e8e695b1622b15c35128082c11f6a04da8
Abi <amariano.fw@gmail.com>
2024-05-31 11:23:26 +0800

    Initial Commit

---
+++ SwiftUIAuth/SwiftUIAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthApp.swift
+//  SwiftUIAuth
+//
+//  Created by Abi M on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
