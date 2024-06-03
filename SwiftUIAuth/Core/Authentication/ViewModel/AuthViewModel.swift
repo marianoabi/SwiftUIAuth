a2a4d30933da1f1ea9ebbc5f7f04bbbbf8fe70dd
Abi <amariano.fw@gmail.com>
2024-06-03 13:05:34 +0800

    Integrated fetch user Firestore API and loaded the fetch data in ProfileView

--- SwiftUIAuth/Core/Authentication/ViewModel/AuthViewModel.swift
+++ SwiftUIAuth/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -20,12 +20,22 @@ class AuthViewModel: ObservableObject {
 
     init() {
         self.userSession = Auth.auth().currentUser
+
+        Task {
+            await fetchUser()
+        }
     }
 }
 
 extension AuthViewModel {
     func signIn(withEmail email: String, password: String) async throws {
-        print("Sign in..")
+        do {
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            self.userSession = result.user
+            await fetchUser()
+        } catch {
+            print("DEBUG: Failed to login with error: \(error.localizedDescription)")
+        }
     }
 
     func createUser(withEmail email: String, password: String, fullname: String) async throws {
@@ -35,16 +45,30 @@ extension AuthViewModel {
             let user = User(id: result.user.uid, fullName: fullname, email: email)
             let encodedUser = try Firestore.Encoder().encode(user)
             try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+            await fetchUser()
         } catch {
             print("DEBUG: Failed to create user with error: \(error.localizedDescription)")
         }
     }
 
     func signOut() {
-
+        do {
+            try Auth.auth().signOut()
+            self.userSession = nil
+            self.currentUser = nil
+        } catch {
+            print("DEBUG: Failed to signout with error: \(error.localizedDescription)")
+        }
     }
 
     func deleteAccount() {
 
+    }
+
+    func fetchUser() async {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+
+        guard let snapshot = try? await Firestore.firestore().collection("users").document(uid).getDocument() else { return }
+        self.currentUser = try? snapshot.data(as: User.self)
     }
 }
--- SwiftUIAuth/Core/Profile/ProfileView.swift
+++ SwiftUIAuth/Core/Profile/ProfileView.swift
@@ -8,53 +8,57 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+
     var body: some View {
-        List {
-            Section {
-                HStack {
-                    Text(User.MOCK_USER.initials)
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.white)
-                        .frame(width: 72, height: 72)
-                        .background(Color(.systemGray3))
-                    .clipShape(Circle())
-
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(User.MOCK_USER.fullName)
-                            .font(.subheadline)
+        if let user = viewModel.currentUser {
+            List {
+                Section {
+                    HStack {
+                        Text(user.initials)
+                            .font(.title)
                             .fontWeight(.semibold)
-                            .padding(.top, 4)
+                            .foregroundStyle(.white)
+                            .frame(width: 72, height: 72)
+                            .background(Color(.systemGray3))
+                        .clipShape(Circle())
 
-                        Text(User.MOCK_USER.email)
-                            .font(.footnote)
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(user.fullName)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+
+                            Text(user.email)
+                                .font(.footnote)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+
+                Section("General") {
+                    HStack {
+                        SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
+                        Spacer()
+                        Text("1.0.0")
+                            .font(.subheadline)
                             .foregroundStyle(.gray)
                     }
                 }
-            }
 
-            Section("General") {
-                HStack {
-                    SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
-                    Spacer()
-                    Text("1.0.0")
-                        .font(.subheadline)
-                        .foregroundStyle(.gray)
+                Section("Account") {
+                    Button(action: {
+                        viewModel.signOut()
+                    }, label: {
+                        SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: .red)
+                    })
+
+                    Button(action: {
+                        print("Deleting account..")
+                    }, label: {
+                        SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: .red)
+                    })
                 }
-            }
-
-            Section("Account") {
-                Button(action: {
-                    print("Sign out..")
-                }, label: {
-                    SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: .red)
-                })
-
-                Button(action: {
-                    print("Deleting account..")
-                }, label: {
-                    SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: .red)
-                })
             }
         }
     }
